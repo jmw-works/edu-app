90aefdaba0b337d7c6819b0f4a610bc143005822
J.M. Wilyat <jmwilyat@gmail.com>
2025-08-05 00:34:42 -0700

    update

--- src/types/QuestionTypes.ts
+++ src/types/QuestionTypes.ts
@@ -1,12 +1,11 @@
 // src/types/QuestionTypes.ts
 
-// Make sure this path matches your local codegen output!
-// If the file below does not exist, run: npx ampx codegen
-import type { Schema } from '../amplify/data/resource';
+import { Schema } from 'amplify/data/resource';
 
 export type QuestionWithAnswers = Omit<Schema['Question']['type'], 'answers'> & {
   answers: Schema['Answer']['type'][];
 };
+
 
 
 
